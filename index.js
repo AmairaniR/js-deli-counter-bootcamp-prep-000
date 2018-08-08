dc153d0b11a971714e5b55e670f9173ed4a5a4b7
amarsr13686@gmail.com <amarsr13686@gmail.com>
2018-08-08 00:25:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var katzDeliLine = []
+
+function takeANumber(katzDeliLine, names) {
+  katzDeliLine.push(names)
+}
+
+function nowServing(katzDeliLine) {
+  return `Currently serving ${katzDeliLine.length[0]}`
+  var i = 0
+  while (i < katzDeliLine.length) {
+    katzDeliLine.push()
+  }
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served."
+  }
+}
